b770b365de5549a4f2150c75f62c251eacc36b6d
stewartchloe13 <stewartchloe13@gmail.com>
2017-09-06 11:27:23 -0400

    favoriteFood

---
+++ GettingStarted/FavoriteFood/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavoriteFood
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your favorite food?");
+            var favoriteFood = Console.ReadLine();
+
+            if (favoriteFood == "pizza")
+            {
+                Console.WriteLine("Most excellent and correct!Pizza is indeed one of the best foods ever put on this earth.");
+            }
+        }
+    }
+}
