449b35c6c9657104e23dd916ada5576e8df1e7b3
stewartchloe13@gmail.com <chloes22>
2017-09-14 10:54:30 -0400

    DecimalToBinary

---
+++ DecimalToBinary/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number: ");
+            int decimalNumber = Convert.ToInt32(Console.ReadLine());
+
+            int remainder;
+            string result = string.Empty;
+            while (decimalNumber > 0)
+            {
+                remainder = decimalNumber % 2;
+                decimalNumber /= 2;
+                result = remainder.ToString() + result;
+            }
+            Console.WriteLine("Binary:  {0}", result.PadLeft(8, '0'));
+        }
+    }
+}
