5dce02ffbc9f44769d0f267d8743258dc793caaa
stewartchloe13 <stewartchloe13@gmail.com>
2017-10-19 11:27:14 -0400

    aksdjfkl

---
+++ max2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace max2
+{
+    class max2
+    {
+        static void Main(string[] args)
+        { 
+                int min = 0;
+                int max = 100000;
+
+                int[] numbers = new int[5000];
+
+                Random r = new Random();
+                for (int i = 0; i < numbers.Length; i++)
+                { numbers[i] = r.Next(min, max); }
+
+                int max2 = numbers.Max();
+                Console.WriteLine("The largest number in the array is: " + max2);
+                int secondHighest = (from number in numbers orderby number descending select number).Distinct().Skip(1).First();
+                Console.WriteLine("The second largest number in the array is: " + secondHighest);
+            }
+        }
+    }
+
+
